bfe758bec51fe1e37a30f5bcf6796a8f0e6ccb1d
SadarulkOfficial <botdeploy179@gmail.com>
2024-11-27 07:49:06 +0530

    Update img2url.js

--- plugins/img2url.js
+++ plugins/img2url.js
@@ -14,12 +14,11 @@ try{
 const config = await readEnv();
 if(config.BLOCK_JID.includes(from)) return
 
-const isQuotedViewOnce = m.quoted ? (m.quoted.type === 'viewOnceMessage') : false
+
     const isQuotedImage = m.quoted ? ((m.quoted.type === 'imageMessage') || (isQuotedViewOnce ? (m.quoted.msg.type === 'imageMessage') : false)) : false
-    const isQuotedVideo = m.quoted ? ((m.quoted.type === 'videoMessage') || (isQuotedViewOnce ? (m.quoted.msg.type === 'videoMessage') : false)) : false
-    const isQuotedSticker = m.quoted ? (m.quoted.type === 'stickerMessage') : false
+
      if ((m.type === 'imageMessage') || isQuotedImage) {
-      var nameJpg = `sticker`
+      var nameJpg = `infinitywabot`
       isQuotedImage ? await m.quoted.download(nameJpg) : await m.download(nameJpg)
 
 let data = await image2url(nameJpg + '.jpg');
@@ -29,6 +28,8 @@ let msg = `*Url :* ${data.result.url}
 > ɪɴꜰɪɴɪᴛʏ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ ᴄʀᴇᴀᴛᴇᴅ ʙʏ ꜱᴀᴅᴀʀᴜ`
 
 reply(`${msg}`)
+     } else {
+reply("*_Please reply an image._*")
      }
 }catch(e){
 console.log(e)
