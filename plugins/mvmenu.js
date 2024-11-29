744e3cd8c0b78c11b1604319fb3a67ff1d86326e
SadarulkOfficial <botdeploy179@gmail.com>
2024-11-29 17:41:27 +0530

    Add files via upload

---
+++ plugins/mvmenu.js
@@ -0,0 +1,42 @@
+const {readEnv} = require('../lib/database')
+const {cmd , commands} = require('../command')
+
+cmd({
+    pattern: "mvmenu",
+    desc: "Get movie menu",
+    category: "owner",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+
+const config = await readEnv();
+if(config.BLOCK_JID.includes(from)) return
+if(!isOwner) return reply("*_This an owner cmd._*")
+
+let msg = `*👋 HELLO _${pushname}_*
+
+「 ɪɴꜰɪɴɪᴛʏ ᴍᴏᴠɪᴇ ᴍᴇɴᴜ 」
+
+╭──────────●
+│❯ MOVIE COMMANDS ❮
+│   ───────
+│► .movie
+│► .mvsearch
+│► .downlink
+│► .mvsend
+│► .infosend
+│► .dirsend
+╰───────────●
+
+> ɪɴꜰɪɴɪᴛʏ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ ᴄʀᴇᴀᴛᴇᴅ ʙʏ ꜱᴀᴅᴀʀᴜ`
+
+let menuImg = `https://github.com/Sadarulk/QueenMatheeDB/blob/main/botlogos/infinitybotlogo.png?raw=true`
+
+await conn.sendMessage(from,{image:{url: menuImg },caption:msg},{quoted:mek})
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
