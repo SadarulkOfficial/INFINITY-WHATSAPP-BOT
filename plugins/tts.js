51ea61fb4dd5a74317b5a1b0339c4d63a16a9e33
SadarulkOfficial <botdeploy179@gmail.com>
2024-11-29 18:07:36 +0530

    Update tts.js

--- plugins/tts.js
+++ plugins/tts.js
@@ -22,7 +22,7 @@ const url = googleTTS.getAudioUrl(q, {
   host: 'https://translate.google.com',
 });
 
-await conn.sendMessage(from,{audio:{url: url},{quoted:mek})
+await conn.sendMessage(from,{audio:{url: url}},{quoted:mek})
         
 }catch(e){
 console.log(e)
