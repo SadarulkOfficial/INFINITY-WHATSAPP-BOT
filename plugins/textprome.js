0a38b2f4a9bd8cf772936d284a081f64b7934b26
SadarulkOfficial <botdeploy179@gmail.com>
2024-11-29 16:31:16 +0530

    Update textprome.js

--- plugins/textprome.js
+++ plugins/textprome.js
@@ -1,12 +1,11 @@
 const {cmd , commands} = require('../command')
 const {readEnv} = require('../lib/database')
-const TextPro = require('@sl-code-lords/text-pro-me')
-const text_pro = new TextPro()
+const { screenshotV3 } = require('getscreenshot.js')
 
 cmd({
-    pattern: "phub",
-    desc: "Create logo",
-    category: "logo",
+    pattern: "ss",
+    desc: "Get screenshots",
+    category: "other",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
@@ -15,8 +14,8 @@ try{
 const config = await readEnv();
 if(config.BLOCK_JID.includes(from)) return
 
-var image2 = await text_pro.double_text_create('https://textpro.me/generate-a-free-logo-in-pornhub-style-online-977.html','Infinity','Bot')
-    console.log(image2)
+let data = await screenshotV3('https://github.com/')
+console.log(data)
   
 }catch(e){
 console.log(e)
