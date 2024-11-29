12e38b7ed212a80c918959cb1f3dcb2fcb4459c4
SadarulkOfficial <botdeploy179@gmail.com>
2024-11-29 21:10:08 +0530

    Update translator.js

--- plugins/translator.js
+++ plugins/translator.js
@@ -14,18 +14,28 @@ async(conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, send
 const config = await readEnv();
 if(config.BLOCK_JID.includes(from)) return
 
-if(!q) return reply('*_Please give me a text._*')
-
-let a = q.split("&")
+let a = q.split(" ")
 let b = a[0]
 let c = a[1]
 let d = a[2]
+
+if(!m.quoted && q) {
         
 translatte(d, {from: b ,to: c}).then(res => {
-    console.log(res.text);
     reply(res.text);
 })
-        
+
+} else if(m.quoted && b && c) {
+
+translatte(m.quoted, {from: b ,to: c}).then(res => {
+    reply(res.text);
+})
+    
+} else {
+
+reply('*_Please give me a text._*\n\n*Ex:-* .tr <current language> <translate language> <text>') 
+    
+}
 }catch(e){
 console.log(e)
 reply(`${e}`)
