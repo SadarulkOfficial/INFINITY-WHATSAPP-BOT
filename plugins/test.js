450ac27c94243dd1734b8332c04c2d5d23bb76d0
SadarulkOfficial <botdeploy179@gmail.com>
2024-11-30 21:32:04 +0530

    Update and rename alivetest.js to test.js

---
+++ plugins/test.js
@@ -0,0 +1,29 @@
+const {cmd , commands} = require('../command')
+const {readEnv} = require('../lib/database')
+
+cmd({
+    pattern: "test",
+    desc: "test",
+    category: "owner",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+  
+const config = await readEnv();
+if(config.BLOCK_JID.includes(from)) return
+if(!isOwner) return reply("*_This is an owner cmd._*")
+
+await conn.sendMessage(from, {text : 'Hello World !' },{quoted : mek})
+
+if(mek.body === '1') {
+
+reply(`This is test`)
+  
+}
+  
+}catch(e){
+l(e)
+reply(`${e}`)
+}
+})
