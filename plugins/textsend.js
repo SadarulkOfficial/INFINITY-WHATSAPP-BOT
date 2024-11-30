299f9b5aa3b176cfceb94a314063aac8b5cf73dc
SadarulkOfficial <botdeploy179@gmail.com>
2024-12-01 00:10:47 +0530

    Update and rename test.js to textsend.js

---
+++ plugins/textsend.js
@@ -0,0 +1,25 @@
+const {cmd , commands} = require('../command')
+const {readEnv} = require('../lib/database')
+
+cmd({
+    pattern: "send",
+    desc: "text send to jid",
+    category: "owner",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+  
+const config = await readEnv();
+if(config.BLOCK_JID.includes(from)) return
+if(!isOwner) return reply("*_This is an owner cmd._*")
+if(!m.quoted) return reply("*_Please reply a text msg._*")
+if(!q) return reply("*_Please give me a jid to send this text._*")
+
+await conn.sendMessage(q, { text : m.quoted })
+    
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
