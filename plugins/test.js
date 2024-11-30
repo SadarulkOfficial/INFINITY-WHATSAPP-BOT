0425559db70286888bd85912d6afcdb90eaf84ce
SadarulkOfficial <botdeploy179@gmail.com>
2024-11-30 22:59:10 +0530

    Update test.js

--- plugins/test.js
+++ plugins/test.js
@@ -14,37 +14,24 @@ const config = await readEnv();
 if(config.BLOCK_JID.includes(from)) return
 if(!isOwner) return reply("*_This is an owner cmd._*")
 
-let test5 = `Test footer`;
-    
-const test = [{
-      'title': '',
-      'rows': [{
-        'title': '1',
-        'rowId': "." + "song " + "https://youtube.com/watch?v=FCbdpvexz4Q",
-        'description': "Audio file"
-      }, {
-        'title': '2',
-        'rowId': "." + "video " + "https://youtube.com/watch?v=FCbdpvexz4Q",
-        'description': "Video file"
-      }]
-    }];
-
-let test3 = "Hello there, This is test.";
-
-const test4 = {
-      url: "https://github.com/Sadarulk/QueenMatheeDB/blob/main/botlogos/infinitybotlogo.png?raw=true"
-    };
-    
-await conn.sendMessage(from, {
-      caption: test3,
-      image: test4,
-      footer: test5,
-      title: '',
-      buttonText: "*🔢 Reply below number*",
-      sections: test
-    }, {
-      quoted: mek
-    });
+const msg = {
+            newsletterJid: "120363314182963253@newsletter",
+            newsletterName: "乡𝔸𝕊𝕀𝕋ℍ𝔸 𝕞𝕕 ࿐",
+            serverMessageId: 999
+          };
+          const test1 = {
+            mentionedJid: [m.sender],
+            forwardingScore: 999,
+            isForwarded: true,
+            forwardedNewsletterMessageInfo: msg
+          };
+          const test2 = {
+            text: "Hello there.",
+            contextInfo: test1
+          };
+         await conn.sendMessage(from, test2, {
+            'quoted': mek
+          })
     
 }catch(e){
 console.log(e)
