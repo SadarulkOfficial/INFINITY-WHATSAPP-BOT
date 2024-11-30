762b02ed0c8d5e9ef73e6454a40236f00c6e0f10
SadarulkOfficial <botdeploy179@gmail.com>
2024-11-30 12:24:30 +0530

    Update moviesend2.js

--- plugins/moviesend2.js
+++ plugins/moviesend2.js
@@ -53,7 +53,7 @@ if (filteredLinks.length === 0) {
 
         const downloadUrl = filteredLinks[0].downloadDetails.DIRECT_LINK
 
-               const caption = `${mv.data.title} ( ${filteredLinks.quality} )\n\n> ɪɴꜰɪɴɪᴛʏ ᴍᴏᴠɪᴇ ᴡᴏʀʟᴅ`
+               const caption = `${mv.data.title} ( ${filteredLinks[0].quality} )\n\n> ɪɴꜰɪɴɪᴛʏ ᴍᴏᴠɪᴇ ᴡᴏʀʟᴅ`
 
 if(!sendJid) {
 
@@ -80,6 +80,46 @@ await conn.sendMessage(
                       )
        }
         
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
+
+
+cmd({
+    pattern: "mvsearch2",
+    desc: "search movies",
+    filename: __filename
+},
+async(conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+
+const config = await readEnv();
+if(config.BLOCK_JID.includes(from)) return
+
+        if (!isOwner) {
+            return reply(
+                "*_This is an owner cmd_*"
+            )
+        }
+    
+
+const mv = await fetchJson(`${apilink}download/cinesubz-search?q=${q}`)
+
+let array = mv.data
+
+        if(array.length === 0) {
+return reply(`*_Can't find this movie !_*`)
+        }       
+
+let result = array.map((movie, index) => `${index + 1}. *Movie Name :* ${array[index].title}\n*Category :* ${array[index].category}\n*Year :* ${array[index].year}\n*Link :* ${array[index].link}`).join("\n\n");
+
+let dt = `*_INFINITY WA BOT MOVIE SEARCH 🔎_*\n\n`
+let cap = `\n\n> ɪɴꜰɪɴɪᴛʏ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ ᴄʀᴇᴀᴛᴇᴅ ʙʏ ꜱᴀᴅᴀʀᴜ`
+            
+await conn.sendMessage(from, { text: `${dt} ${result} ${cap}` }, {quoted: mek})
+
 }catch(e){
 console.log(e)
 reply(`${e}`)
