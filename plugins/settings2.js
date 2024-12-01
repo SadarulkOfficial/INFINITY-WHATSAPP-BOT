c26a400cd16ac3853c1b42fd348370d72034d24d
SadarulkOfficial <botdeploy179@gmail.com>
2024-12-01 10:30:07 +0530

    Update and rename test.js to settings2.js

---
+++ plugins/settings2.js
@@ -0,0 +1,131 @@
+const { updateEnv, readEnv } = require('../lib/database');
+const { cmd, commands } = require('../command');
+const EnvVar = require('../lib/mongodbenv');
+
+cmd({
+    pattern: "settings2",
+    desc: "Change bot settings",
+    category: "owner",
+    filename: __filename
+}, 
+async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+        if (!isOwner) return reply(*_This is an owner cmd._*)
+
+        const config = await readEnv();
+
+let settingMsg = `Infinity whatsapp bot settings ⚙
+
+*➤ BOT WORK TYPE*
+
+┌───────────────────
+├ 1.1 || _PUBLIC_
+├ 1.2 || _PRIVATE_
+├ 1.3 || _ONLY GROUPS_
+├ 1.4 || _ONLY INBOX_
+└───────────────────
+
+*➤ AUTO STATUS SEEN*
+
+┌───────────────────
+├ 2.1 || _ON_
+├ 2.2 || _OFF_
+└───────────────────
+
+*➤ AUTO REPLY*
+
+┌───────────────────
+├ 3.1 || _ON_
+├ 3.2 || _OFF_
+└───────────────────
+
+*➤ AUTO VOICE*
+
+┌───────────────────
+├ 4.1 || _ON_
+├ 4.2 || _OFF_
+└───────────────────
+
+*➤ AUTO STICKER*
+
+┌───────────────────
+├ 5.1 || _ON_
+├ 5.2 || _OFF_
+└───────────────────
+
+*➤ AUTO AI*
+
+┌───────────────────
+├ 6.1 || _ON_
+├ 6.2 || _OFF_
+└───────────────────
+
+> ɪɴꜰɪɴɪᴛʏ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ ᴄʀᴇᴀᴛᴇᴅ ʙʏ ꜱᴀᴅᴀʀᴜ`
+
+        const msg2 = await conn.sendMessage(from, {image: { url: 'https://github.com/Sadarulk/QueenMatheeDB/blob/main/botlogos/infinitybotlogo.png?raw=true' },caption: settingMsg}, { quoted: mek })
+
+        conn.ev.on('messages.upsert', async (msgUpdate) => {
+            const msg = msgUpdate.messages[0];
+            if (!msg.message || !msg.message.extendedTextMessage) return;
+
+            const selectedOption = msg.message.extendedTextMessage.text.trim();
+
+            if (msg.message.extendedTextMessage.contextInfo && msg.message.extendedTextMessage.contextInfo.stanzaId === msg2.key.id) {
+                switch (selectedOption) {
+                    case '1.1':
+                        reply('.update MODE:public');
+                        reply('.restart');
+                        break;
+                    case '1.2':
+                        reply('.update MODE:private');
+                        reply('.restart');
+                        break;
+                    case '1.3':
+                        reply('.update MODE:only_groups');
+                        reply('.restart');
+                        break;
+                    case '1.4':
+                        reply('.update MODE:inbox');
+                        reply('.restart');
+                        break;
+                    case '2.1':
+                        reply('.update AUTO_READ_STATUS:true');
+                        break;
+                    case '2.2':
+                        reply('.update AUTO_READ_STATUS:false');
+                        break;
+                    case '3.1':
+                        reply('.update AUTO_REPLY:true');
+                        break;
+                    case '3.2':
+                        reply('.update AUTO_REPLY:false');
+                        break;
+                    case '4.1':
+                        reply('.update AUTO_VOICE:true');
+                        break;
+                    case '4.2':
+                        reply('..update AUTO_VOICE:false');
+                        break;
+                    case '5.1':
+                        reply('.update AUTO_STICKER:true');
+                        break;
+                    case '5.2':
+                        reply('.update AUTO_STICKER:false');
+                        break;
+                    case '6.1':
+                        reply('.update AUTO_AI:true');
+                        break;
+                    case '6.2':
+                        reply('.update AUTO_AI:false');
+                        break;
+                    default:
+                        reply("*_Invalid number.Please reply a valid number_*");
+                }
+            }
+        });
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
