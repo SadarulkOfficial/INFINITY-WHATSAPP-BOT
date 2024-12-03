1f8ee7c00ab179fc91a68762eddcf61aa741f85e
SadarulkOfficial <botdeploy179@gmail.com>
2024-12-03 15:16:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=azB0GB4C#YC-bNtFnrUiXs7UPNxcsgp1DpwM190rKwNFE7s1jBgE",
-MONGODB: process.env.MONGODB || "mongodb://mongo:eurOZzzjLqxxITWuuVDoKCpsvFvvAYop@autorack.proxy.rlwy.net:47304",
+SESSION_ID: process.env.SESSION_ID || "Enter your session id here",
+MONGODB: process.env.MONGODB || "Enter your mongoDB here",
 };
