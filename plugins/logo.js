041433ee0e575cdfe13d1ac7d26b12a1d139383c
SadarulkOfficial <botdeploy179@gmail.com>
2024-12-05 23:09:18 +0530

    Create logo.js

---
+++ plugins/logo.js
@@ -0,0 +1,153 @@
+const {readEnv} = require('../lib/database')
+const {cmd , commands} = require('../command')
+const { fetchJson } = require('../lib/functions')
+
+const apilink = 'https://api-pink-venom.vercel.app'
+const caption = `> ɪɴꜰɪɴɪᴛʏ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ ᴄʀᴇᴀᴛᴇᴅ ʙʏ ꜱᴀᴅᴀʀᴜ`
+
+let logo1 = 'https://en.ephoto360.com/create-a-blackpink-style-logo-with-members-signatures-810.html'
+let logo2 = `https://en.ephoto360.com/online-blackpink-style-logo-maker-effect-711.html`
+let logo3 = `https://en.ephoto360.com/create-glossy-silver-3d-text-effect-online-802.html`
+let logo4 = `https://en.ephoto360.com/naruto-shippuden-logo-style-text-effect-online-808.html`
+let logo5 = `https://en.ephoto360.com/create-digital-glitch-text-effects-online-767.html`
+let logo6 = `https://en.ephoto360.com/create-pixel-glitch-text-effect-online-769.html`
+let logo7 = `https://en.ephoto360.com/create-online-3d-comic-style-text-effects-817.html`
+let logo8 = `https://en.ephoto360.com/create-colorful-neon-light-text-effects-online-797.html`
+let logo9 = `https://en.ephoto360.com/free-bear-logo-maker-online-673.html`
+let logo10 = `https://en.ephoto360.com/neon-devil-wings-text-effect-online-683.html`
+
+cmd({
+    pattern: "logo",
+    desc: "Create logos",
+    category: "other",
+    filename: __filename
+},
+async(conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+
+const config = await readEnv()
+if(config.BLOCK_JID.includes(from)) return
+if(!q) return reply("*_Please give me a text._*")
+  
+let data = await fetchJson(`${apilink}/api/logo?url=${logo1}&name=${q}`)
+
+let logoMsg = `*_INFINITY WA BOT LOGO MAKER_*
+
+───────────────────
+*Text :* ${q}
+───────────────────
+
+_🔢 Reply Below Number :_
+
+ 1 || Black Pink
+ 2 || Black Pink 2
+ 3 || Silver 3D
+ 4 || Naruto
+ 5 || Digital Glitch
+ 6 || Pixel Glitch
+ 7 || Comic Style
+ 8 || Neon Light
+ 9 || Free Bear
+10 || Devil Wings
+
+> ɪɴꜰɪɴɪᴛʏ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ ᴄʀᴇᴀᴛᴇᴅ ʙʏ ꜱᴀᴅᴀʀᴜ`
+
+const fdChannel = {
+            newsletterJid: "120363352976453510@newsletter",
+            newsletterName: "INFINITY WA BOT",
+            serverMessageId: 999
+          };
+          const contextMsg = {
+            mentionedJid: [m.sender],
+            forwardingScore: 999,
+            isForwarded: true,
+            forwardedNewsletterMessageInfo: fdChannel
+          };
+          const msgBody = {
+            text: logoMsg,
+            contextInfo: contextMsg
+          };
+        let send = await conn.sendMessage(from, msgBody, {
+            'quoted': mek
+          })
+
+conn.ev.on('messages.upsert', async (msgUpdate) => {
+            const msg = msgUpdate.messages[0];
+            if (!msg.message || !msg.message.extendedTextMessage) return;
+
+            const selectedOption = msg.message.extendedTextMessage.text.trim();
+
+            if (msg.message.extendedTextMessage.contextInfo && msg.message.extendedTextMessage.contextInfo.stanzaId === send.key.id) {
+                switch (selectedOption) {
+                    case '1':
+        
+let data = await fetchJson(`${apilink}/api/logo?url=${logo1}&name=${q}`)
+await conn.sendMessage(from, { image :{url : `${data.result.download_url}`}, caption : `${caption}`},{quoted : mek})
+
+                        break;
+                    case '2':
+
+let data = await fetchJson(`${apilink}/api/logo?url=${logo2}&name=${q}`)
+await conn.sendMessage(from, { image :{url : `${data.result.download_url}`}, caption : `${caption}`},{quoted : mek})
+                    
+                    break;
+                    case '3':
+
+let data = await fetchJson(`${apilink}/api/logo?url=${logo3}&name=${q}`)
+await conn.sendMessage(from, { image :{url : `${data.result.download_url}`}, caption : `${caption}`},{quoted : mek})
+                    
+                    break;
+                    case '4':
+
+let data = await fetchJson(`${apilink}/api/logo?url=${logo4}&name=${q}`)
+await conn.sendMessage(from, { image :{url : `${data.result.download_url}`}, caption : `${caption}`},{quoted : mek})
+                    
+                    break;
+                    case '5':
+
+let data = await fetchJson(`${apilink}/api/logo?url=${logo5}&name=${q}`)
+await conn.sendMessage(from, { image :{url : `${data.result.download_url}`}, caption : `${caption}`},{quoted : mek})
+                    
+                    break;
+                    case '6':
+
+let data = await fetchJson(`${apilink}/api/logo?url=${logo6}&name=${q}`)
+await conn.sendMessage(from, { image :{url : `${data.result.download_url}`}, caption : `${caption}`},{quoted : mek})
+                    
+                    break;
+                    case '7':
+
+let data = await fetchJson(`${apilink}/api/logo?url=${logo7}&name=${q}`)
+await conn.sendMessage(from, { image :{url : `${data.result.download_url}`}, caption : `${caption}`},{quoted : mek})
+                    
+                    break;
+                    case '8':
+
+let data = await fetchJson(`${apilink}/api/logo?url=${logo8}&name=${q}`)
+await conn.sendMessage(from, { image :{url : `${data.result.download_url}`}, caption : `${caption}`},{quoted : mek})
+                    
+                    break;
+                    case '9':
+
+let data = await fetchJson(`${apilink}/api/logo?url=${logo9}&name=${q}`)
+await conn.sendMessage(from, { image :{url : `${data.result.download_url}`}, caption : `${caption}`},{quoted : mek})
+                    
+                    break;
+                    case '10':
+
+let data = await fetchJson(`${apilink}/api/logo?url=${logo10}&name=${q}`)
+await conn.sendMessage(from, { image :{url : `${data.result.download_url}`}, caption : `${caption}`},{quoted : mek})
+                    
+                    break;
+                    default:
+                        reply("*_Invalid number.Please reply a valid number._*");
+                }
+
+            }
+        })
+                        
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
