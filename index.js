b3cfe546e981f8547df21ca1e2fc032b7018d924
SadarulkOfficial <botdeploy179@gmail.com>
2024-12-06 09:34:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -107,7 +107,9 @@ if (!mek.message) return
 mek.message = (getContentType(mek.message) === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
 if (mek.key && mek.key.remoteJid === 'status@broadcast' && config.AUTO_READ_STATUS === "true"){
 await conn.readMessages([mek.key])
+await conn.sendMessage(mek.key.remoteJid, { react: { text: "〽️" }})
 }
+   
 const m = sms(conn, mek)
 const type = getContentType(mek.message)
 const content = JSON.stringify(mek.message)
