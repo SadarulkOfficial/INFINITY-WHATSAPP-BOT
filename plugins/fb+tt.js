1ba847434b03a92734d5378f79c286a0bb9eab61
SadarulkOfficial <botdeploy179@gmail.com>
2024-12-06 17:20:54 +0530

    Update fb+tt.js

--- plugins/fb+tt.js
+++ plugins/fb+tt.js
@@ -20,27 +20,64 @@ async(conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, send
          if (!data.result) {
             return reply("*_Can't download your facebook video._*");
         }
-        
-const response = await conn.sendMessage(from, { text: '_*Your facebook video is downloading... 📥*_' }, {quoted : mek});
 
-        
-        const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+let desc = `*_INFINITY WA BOT FACEBOOK DOWNLOADER_* 📥
 
-       
-        await delay(1000);
+🖇️ *Link:* ${data.result.url}
 
-        
-            await conn.sendMessage(from, {
-            text: '_*Your facebook video is uploading... 📤*_',
-            edit: response.key,
-        });
-        
-        await delay(1000);
+🔢 Reply Below Number :
 
-            await conn.sendMessage(from, { delete: response.key });
-        
-        await conn.sendMessage(from,{video: {url:data.result.hd},mimetype:"video/mp4",caption: `*HD Quality*\n\n> ɪɴꜰɪɴɪᴛʏ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ` },{quoted: mek})
-        await conn.sendMessage(from,{video: {url:data.result.sd},mimetype:"video/mp4",caption: `*SD Quality*\n\n> ɪɴꜰɪɴɪᴛʏ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ` },{quoted: mek})
+1 || HD Quality
+2 || SD Quality
+
+> ɪɴꜰɪɴɪᴛʏ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ ᴄʀᴇᴀᴛᴇᴅ ʙʏ ꜱᴀᴅᴀʀᴜ`
+
+const fdChannel = {
+            newsletterJid: "120363352976453510@newsletter",
+            newsletterName: "INFINITY WA BOT",
+            serverMessageId: 999
+          };
+          const contextMsg = {
+            mentionedJid: [m.sender],
+            forwardingScore: 999,
+            isForwarded: true,
+            forwardedNewsletterMessageInfo: fdChannel
+          };
+          const msgBody = {
+            image : { url : data.result.thumbnail },
+            caption : desc,
+            contextInfo: contextMsg
+          };
+         let inf = await conn.sendMessage(from, msgBody, {
+            'quoted': mek
+          })
+
+//==========number reply==========
+
+conn.ev.on('messages.upsert', async (msgUpdate) => {
+            const msg = msgUpdate.messages[0];
+            if (!msg.message || !msg.message.extendedTextMessage) return;
+
+            const selectedOption = msg.message.extendedTextMessage.text.trim();
+
+            if (msg.message.extendedTextMessage.contextInfo && msg.message.extendedTextMessage.contextInfo.stanzaId === inf.key.id) {
+                switch (selectedOption) {
+                    case '1':
+
+                        await conn.sendMessage(from,{video: {url:data.result.hd},mimetype:"video/mp4",caption: `*HD Quality*\n\n> ɪɴꜰɪɴɪᴛʏ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ` },{quoted: mek})
+                        
+                        break; 
+                    case '2':
+
+                        await conn.sendMessage(from,{video: {url:data.result.sd},mimetype:"video/mp4",caption: `*SD Quality*\n\n> ɪɴꜰɪɴɪᴛʏ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ` },{quoted: mek})
+
+                        break;
+                    default:
+                        reply("*_Invalid number.Please reply a valid number._*");
+                }
+
+            }
+        })
 
     } catch(e) {
       console.log(e)
@@ -61,34 +98,81 @@ async(conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, send
 
         if (!q && !q.startsWith("https://")) return reply("*_Please give me a tiktok url._*")
 
-        
         let data = await fetchJson(`https://www.dark-yasiya-api.site/download/tiktok?url=${q}`)
 
          if (!data.result) {
             return reply("*_Can't download your tiktok video._*");
         }
         
-const response = await conn.sendMessage(from, { text: '_*Your tiktok video is downloading... 📥*_' }, {quoted : mek});
+let desc = `*_INFINITY WA BOT TIKTOK DOWNLOADER_* 📥
 
-        
-        const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+┌───────────────────
+├ ℹ️ *Title:* ${data.result.title}
+├ 👤 *Author:* ${data.result.author}
+├ 👁️‍🗨️ *Views:* ${data.result.views}
+├ 🕘 *Duration:* ${data.result.duration}
+├ 🖇️ *Link:* ${q}
+└───────────────────
 
-       
-        await delay(1000);
+🔢 Reply Below Number :
 
-        
-            await conn.sendMessage(from, {
-            text: '_*Your tiktok video is uploading... 📤*_',
-            edit: response.key,
-        });
-        
-        await delay(1000);
+1 || Without WaterMark
+2 || With WaterMark
+3 || Audio
 
-            await conn.sendMessage(from, { delete: response.key });
-        
-        await conn.sendMessage(from,{video: {url:data.result.hdVideo},mimetype:"video/mp4",caption: `*Without WaterMark*\n\n> ɪɴꜰɪɴɪᴛʏ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ` },{quoted: mek})
-        await conn.sendMessage(from,{video: {url:data.result.wmVideo},mimetype:"video/mp4",caption: `*With WaterMark*\n\n> ɪɴꜰɪɴɪᴛʏ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ
-` },{quoted: mek})
+> ɪɴꜰɪɴɪᴛʏ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ ᴄʀᴇᴀᴛᴇᴅ ʙʏ ꜱᴀᴅᴀʀᴜ`
+
+const fdChannel = {
+            newsletterJid: "120363352976453510@newsletter",
+            newsletterName: "INFINITY WA BOT",
+            serverMessageId: 999
+          };
+          const contextMsg = {
+            mentionedJid: [m.sender],
+            forwardingScore: 999,
+            isForwarded: true,
+            forwardedNewsletterMessageInfo: fdChannel
+          };
+          const msgBody = {
+            image : { url : data.result.cover },
+            caption : desc,
+            contextInfo: contextMsg
+          };
+         let inf = await conn.sendMessage(from, msgBody, {
+            'quoted': mek
+          })
+
+//==========number reply==========
+
+conn.ev.on('messages.upsert', async (msgUpdate) => {
+            const msg = msgUpdate.messages[0];
+            if (!msg.message || !msg.message.extendedTextMessage) return;
+
+            const selectedOption = msg.message.extendedTextMessage.text.trim();
+
+            if (msg.message.extendedTextMessage.contextInfo && msg.message.extendedTextMessage.contextInfo.stanzaId === inf.key.id) {
+                switch (selectedOption) {
+                    case '1':
+
+                        await conn.sendMessage(from,{video: {url:data.result.hdVideo},mimetype:"video/mp4",caption: `*Without WaterMark*\n\n> ɪɴꜰɪɴɪᴛʏ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ` },{quoted: mek})
+                        
+                        break; 
+                    case '2':
+
+                         await conn.sendMessage(from,{video: {url:data.result.wmVideo},mimetype:"video/mp4",caption: `*With WaterMark*\n\n> ɪɴꜰɪɴɪᴛʏ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ` },{quoted: mek})
+
+                        break;
+                    case '3':
+
+                         await conn.sendMessage(from,{audio: {url:data.result.sound},mimetype:"audio/mpeg"},{quoted: mek})
+
+                        break;
+                    default:
+                        reply("*_Invalid number.Please reply a valid number._*");
+                }
+
+            }
+        })
 
     } catch(e) {
       console.log(e)
