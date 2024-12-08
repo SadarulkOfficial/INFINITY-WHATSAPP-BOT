655ee0fa0bf11261bf80517d688b0c1f5890c859
SadarulkOfficial <botdeploy179@gmail.com>
2024-12-08 19:34:08 +0530

    Update anime.js

--- plugins/anime.js
+++ plugins/anime.js
@@ -1,6 +1,7 @@
 const {readEnv} = require('../lib/database')
 const {cmd , commands} = require('../command')
 const randomChar = require('anime-character-random')
+const { fetchJson } = require('../lib/functions')
 
 cmd({
     pattern: "anime",
@@ -37,6 +38,74 @@ const fdChannel = {
             contextInfo: contextMsg
           };
          await conn.sendMessage(from, msgBody, {
+            'quoted': mek
+          })
+    
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
+
+cmd({
+    pattern: "couple",
+    desc: "Get random couple photo",
+    category: "other",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+
+const config = await readEnv()
+if(config.BLOCK_JID.includes(from)) return
+  
+let data = await fetchJson(`https://api.fgmods.xyz/api/img/couple?apikey=nRHt2lt5`)
+
+let boy = `*_Boy_*
+
+> ɪɴꜰɪɴɪᴛʏ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ ᴄʀᴇᴀᴛᴇᴅ ʙʏ ꜱᴀᴅᴀʀᴜ`
+
+const fdChannel = {
+            newsletterJid: "120363352976453510@newsletter",
+            newsletterName: "INFINITY WA BOT",
+            serverMessageId: 999
+          };
+          const contextMsg = {
+            mentionedJid: [m.sender],
+            forwardingScore: 999,
+            isForwarded: true,
+            forwardedNewsletterMessageInfo: fdChannel
+          };
+          const msgBody = {
+            image: {url: `${data.result.boy}`},
+            caption: boy,
+            contextInfo: contextMsg
+          };
+         await conn.sendMessage(from, msgBody, {
+            'quoted': mek
+          })
+
+let girl = `*_Girl_*
+
+> ɪɴꜰɪɴɪᴛʏ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ ᴄʀᴇᴀᴛᴇᴅ ʙʏ ꜱᴀᴅᴀʀᴜ`
+
+const fdChannel2 = {
+            newsletterJid: "120363352976453510@newsletter",
+            newsletterName: "INFINITY WA BOT",
+            serverMessageId: 999
+          };
+          const contextMsg2 = {
+            mentionedJid: [m.sender],
+            forwardingScore: 999,
+            isForwarded: true,
+            forwardedNewsletterMessageInfo: fdChannel2
+          };
+          const msgBody2 = {
+            image: {url: `${data.result.girl}`},
+            caption: girl,
+            contextInfo: contextMsg2
+          };
+         await conn.sendMessage(from, msgBody2, {
             'quoted': mek
           })
     
