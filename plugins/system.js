be555bb65260101c4265c2be7886f7f8010d8838
SadarulkOfficial <botdeploy179@gmail.com>
2024-12-11 20:55:37 +0530

    Update system.js

--- plugins/system.js
+++ plugins/system.js
@@ -15,21 +15,37 @@ const {readEnv} = require('../lib/database')
 const config = await readEnv();
 if(config.BLOCK_JID.includes(from)) return
     
-let status = `┌───────────────────────
-├ ⏰ *Runtime:-* ${runtime(process.uptime())}
-├ 📟 *Ram usage:-* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
-├ ⚙️ *Platform:-* ${os.hostname()}
-├ 👨‍💻 *Owner:-* Sadaru
-├ 🧬 *Version:-* 1.0
-└───────────────────────
+let status = `┌──────────────────────
+├ ⏰ *Runtime :* ${runtime(process.uptime())}
+├ 📟 *Ram usage :* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
+├ ⚙️ *Platform :* ${os.hostname()}
+├ 👨‍💻 *Owner :* Sadaru
+├ 🧬 *Version :* 1.0
+└──────────────────────
 
 > ɪɴꜰɪɴɪᴛʏ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ ᴄʀᴇᴀᴛᴇᴅ ʙʏ ꜱᴀᴅᴀʀᴜ`
 
-return reply(`${status}`)
-
+const fdChannel = {
+            newsletterJid: "120363352976453510@newsletter",
+            newsletterName: "INFINITY WA BOT",
+            serverMessageId: 999
+          };
+          const contextMsg = {
+            mentionedJid: [m.sender],
+            forwardingScore: 999,
+            isForwarded: true,
+            forwardedNewsletterMessageInfo: fdChannel
+          };
+          const msgBody = {
+            text: status,
+            contextInfo: contextMsg
+          };
+         await conn.sendMessage(from, msgBody, {
+            'quoted': mek
+          })
+    
 }catch(e){
 console.log(e)
 reply(`${e}`)
-
 }
 })
