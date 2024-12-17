e6317e74d1b677f1628be62bac9c755b23b72b0a
SadarulkOfficial <botdeploy179@gmail.com>
2024-12-17 21:03:42 +0530

    Update ai.js

--- plugins/ai.js
+++ plugins/ai.js
@@ -93,3 +93,26 @@ console.log(e)
 reply(`${e}`)
 }
 })
+
+cmd({
+    pattern: "codeai",
+    desc: "AI chat feature",
+    category: "ai",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+
+const config = await readEnv()
+if(config.BLOCK_JID.includes(from)) return
+if(!q) return reply("*_Please give me a text._*")
+
+let data = await fetchJson(`https://www.dark-yasiya-api.site/ai/useadrenaline?q=${q}`)
+
+return reply(`${data.result}\n\n> ɪɴꜰɪɴɪᴛʏ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ ᴄʀᴇᴀᴛᴇᴅ ʙʏ ꜱᴀᴅᴀʀᴜ`)
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
